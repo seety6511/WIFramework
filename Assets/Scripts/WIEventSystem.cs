063bdeb98f41274414244e62426ea87655010198
WSHUVC <102495922+WSHUVC@users.noreply.github.com>
2022-09-29 18:00:18 +0900

    versionUpdate
    1.Add WIEventSystem

---
+++ Assets/Scripts/WIEventSystem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+namespace WI.Core
+{
+    /// <summary>
+    /// WICore 의 에디터상 본체(대리) 클래스
+    /// </summary>
+    public class WIEventSystem : MonoBehaviour
+    {
+    }
+}
